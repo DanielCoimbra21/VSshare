e519d181dbb005a3a616360dad6de07460a8288e
Théo Choffat <theo.choffat@hes-so.ch>
2022-01-03 20:41:33 +0100

    server and client

--- vsshare/src/main/java/Client/NewClientConnection.java
+++ vsshare/src/main/java/Client/NewClientConnection.java
@@ -1,96 +1,43 @@
 package Client;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
-public class NewClientConnection{
-
+public class NewClientConnection {
     public static void main(String[] args) {
 
-        Socket clientSocket;
         InetAddress serverAddress;
         String serverName = "127.0.0.1";
-        PrintWriter pout ;
-
-
-        Scanner sc ;
 
         try {
             serverAddress = InetAddress.getByName(serverName);
             System.out.println("Get the address of the server : "+ serverAddress);
 
-            //Ask the server to create a new socket
-            clientSocket = new Socket(serverAddress,12468);
+            //try to connect to the server
+            Socket mySocket = new Socket(serverAddress,45007);
 
-            // écoute le serveur
-            BufferedReader buffin = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())) ;
-            String message_disatnt = buffin.readLine() ;
-            System.out.println(message_disatnt);
+            System.out.println("We got the connexion to  "+ serverAddress);
 
-            //open the output data stream to write on the client
-            pout = new PrintWriter(clientSocket.getOutputStream());
-            //wait for an input from the client
-            sc = new Scanner(System.in);
-            String message = sc.nextLine();
-            //write the message on the output stream
-            pout.println(message);
-            pout.flush();
+            //wait a bit before exit
+            Thread.sleep(30000);
 
+            System.out.println("\nTerminate client program...");
+            mySocket.close();
 
-            // écoute le serveur back
-            buffin = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())) ;
-            message_disatnt = buffin.readLine() ;
-            System.out.println(message_disatnt);
-
-
-            // répond au serveur
-            pout = new PrintWriter(clientSocket.getOutputStream(), true) ;
-            Scanner console = new Scanner(System.in) ;
-            String messageToSend = console.nextLine() ;
-            pout.println(messageToSend);
-
-
-            // écoute le fichier du serveur
-            buffin = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())) ;
-            StringBuffer sb = new StringBuffer();
-            String line;
-            while((line = buffin.readLine()) != null)
-            {
-                // ajoute la ligne au buffer
-                sb.append(line);
-                sb.append("\n");
-            }
-            System.out.println("Contenu du fichier: ");
-            System.out.println(sb.toString());
-
-
-
-            // meurt
-            System.out.println("now dying");
-            clientSocket.close();
 
         }catch (UnknownHostException e) {
 
             e.printStackTrace();
-
         }catch (IOException e) {
-
-            e.printStackTrace();
+            System.out.println("server connection error, dying.....");
+        }catch(NullPointerException e){
+            System.out.println("Connection interrupted with the server");
+        }
+        catch (InterruptedException e) {
+            System.out.println("interrupted exception");
         }
     }
 
-    public int openPorts() throws IOException {
-        int openport = 1 ;
-        for (int i = 44980 ; i < 45010 ; i++){
-            Socket socket = new Socket();
-            socket.connect(new InetSocketAddress("localhost",i),1000);
-            openport = i ;
-            System.out.println("port " + i + "is open");
-        }
-        return openport ;
-    }
 }
--- vsshare/src/main/java/Server/NewServerConnection.java
+++ vsshare/src/main/java/Server/NewServerConnection.java
@@ -21,16 +21,12 @@ public class NewServerConnection {
          */
         public static void main(String[] args) {
 
-
-            ServerSocket mySkServer ;
             Socket srvSocket = null ;
-            InetAddress localAddress=null;
-            String interfaceName = "lo"; //args[0];
-            PrintWriter pout ;
-            String demandeNbLignes ;
-            String linkURL ;
-            String result ;
-            BufferedReader buffin ;
+            InetAddress localAddress = null;
+            ServerSocket mySkServer;
+            String interfaceName = "lo";
+
+            int ClientNo = 1;
 
             try {
                 NetworkInterface ni = NetworkInterface.getByName(interfaceName);
@@ -45,103 +41,27 @@ public class NewServerConnection {
                         }
                     }
                 }
-                String port = "12468" ; //args[1] ;
-                int myPort = Integer.parseInt(port) ;
-                mySkServer = new ServerSocket(myPort,5,localAddress);
 
+                //Warning : the backlog value (2nd parameter is handled by the implementation
+                mySkServer = new ServerSocket(45007,10,localAddress);
                 System.out.println("Default Timeout :" + mySkServer.getSoTimeout());
                 System.out.println("Used IpAddress :" + mySkServer.getInetAddress());
                 System.out.println("Listening to Port :" + mySkServer.getLocalPort());
 
-                mySkServer.setSoTimeout(180000);//set 30 sec timout
-
-                //Listen to a client connection wait until a client connects
-                System.out.println("Waiting for a client connection:");
-                srvSocket = mySkServer.accept();
-                System.out.println("A client is connected");
-
-                // demander l'URL au client
-                //open the output data stream to write on the server
-                pout = new PrintWriter(srvSocket.getOutputStream());
-                //wait for an input from the client
-                linkURL = "Passe moi le lien du fichier que tu veux lire :" ;
-                //write the message on the output stream
-                pout.println(linkURL);
-                pout.flush();
-
-                // récupère l'URL du client
-                buffin = new BufferedReader(new InputStreamReader(srvSocket.getInputStream())) ;
-                String message_distant = buffin.readLine() ;
-                System.out.println(message_distant);
-
-
-
-
-                // va te demander le nombre de lignes que tu veux
-                //open the output data stream to write on the server
-                pout = new PrintWriter(srvSocket.getOutputStream());
-                //wait for an input from the client
-                demandeNbLignes = "tu veux regarder combien de lignes ? :" ;
-                //write the message on the output stream
-                pout.println(demandeNbLignes);
-                pout.flush();
-                // récupère le nb de lignes du client
-                buffin = new BufferedReader(new InputStreamReader(srvSocket.getInputStream())) ;
-                String nblignes = buffin.readLine() ;
-                System.out.println(nblignes);
-
-
-
-
-                // va rechercher le fichier en local et lis le nb de lignes
-                // changer le nb de lignes en int
-                int lignes = Integer.parseInt(nblignes) ;
-                // Le fichier d'entrée
-                File file = new File(message_distant);
-                // Créer l'objet File Reader
-                FileReader fr = new FileReader(file);
-                // Créer l'objet BufferedReader
-                buffin = new BufferedReader(fr);
-                StringBuffer sb = new StringBuffer();
-                String line;
-                //while ((line = br.readLine()) != null) {
-                for(int i = 0 ; i < lignes ; i++) {
-                    // ajoute la ligne au buffer
-                    line = buffin.readLine();
-                    sb.append(line);
-                    sb.append("\n");
+                //wait for a client connection
+                while(true)
+                {
+                    Socket clientSocket = mySkServer.accept();
+                    System.out.println("connection request received");
+                    Thread t = new Thread(new AcceptClient(clientSocket,ClientNo));
+                    ClientNo++;
+                    //starting the thread
+                    t.start();
                 }
-                fr.close();
-                System.out.println("Contenu du fichier: ");
-                System.out.println(sb.toString());
 
-
-
-
-                // envoies le résultat au client
-                //open the output data stream to write on the server
-                pout = new PrintWriter(srvSocket.getOutputStream());
-                //wait for an input from the client
-                result = sb.toString() ;
-                //write the message on the output stream
-                pout.println(result);
-                pout.flush();
-
-
-                //then die
-                System.out.println("now dying");
-                mySkServer.close();
-                srvSocket.close();
-
-            }catch (SocketException e) {
-                //System.out.println("Connection Timed out");
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-
         }
-
-    }
+}
 

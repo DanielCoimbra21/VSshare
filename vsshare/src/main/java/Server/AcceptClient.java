fd0b6238bd3a831191066bc9bb772473081ca0f1
Théo Choffat <theo.choffat@hes-so.ch>
2022-01-03 22:31:44 +0100

    Ask and retrieve the password

--- vsshare/src/main/java/Server/AcceptClient.java
+++ vsshare/src/main/java/Server/AcceptClient.java
@@ -1,16 +1,17 @@
 package Server;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class AcceptClient implements Runnable {
 
     private Socket clientSocketOnServer;
     private int clientNumber;
+    private BufferedReader buffin ;
+    private PrintWriter pout ;
 
     //Constructor
     public AcceptClient (Socket clientSocketOnServer, int clientNo)
@@ -25,25 +26,42 @@ public class AcceptClient implements Runnable {
         try {
             System.out.println("Client Nr "+clientNumber+ " is connected");
             System.out.println("Socket is available for connection"+ clientSocketOnServer);
+            pout = new PrintWriter(clientSocketOnServer.getOutputStream());
 
-            // va rechercher le fichier en local et lis le nb de lignes
-            // changer le nb de lignes en int
-            int lignes = Integer.parseInt(nblignes) ;
-            // Le fichier d'entrée
-            File file = new File(message_distant);
-            // Créer l'objet File Reader
-            FileReader fr = new FileReader(file);
-            // Créer l'objet BufferedReader
-            buffin = new BufferedReader(fr);
-            StringBuffer sb = new StringBuffer();
-            String line;
-            //while ((line = br.readLine()) != null) {
-            for(int i = 0 ; i < lignes ; i++) {
-                // ajoute la ligne au buffer
-                line = buffin.readLine();
-                sb.append(line);
-                sb.append("\n");
+            // va lire le fichier
+            Scanner s = new Scanner(new File("vsshare/src/main/java/RegisteredClients/TheList"));
+            ArrayList<String> list = new ArrayList<String>();
+            while (s.hasNext()){
+                list.add(s.next());
             }
+            s.close();
+
+            // va regarder si l'adresse ip existe déjà
+            String ipAddr = clientSocketOnServer.getInetAddress().toString() ;
+            if(!list.contains(ipAddr)){
+                System.out.println("la liste n'a pas trouvé l'adresse ip");
+                // On va donc envoyer un message au client
+                String premierContact = "Oy tu as l'air nouveau par ici, je vais te créer un compte, quel mot de passe veux-tu ?" ;
+                pout.println(premierContact);
+                pout.flush();
+                // récupère le code du client
+                buffin = new BufferedReader(new InputStreamReader(clientSocketOnServer.getInputStream())) ;
+                String message_distant = buffin.readLine() ;
+                System.out.println(message_distant);
+
+            }
+            else {
+                System.out.println("adresse ip trouvée dans le fichier");
+                // On va donc envoyer un message au client
+                String deuxiemeContact = "REBONJOUR C EST QUOI TON CODE STP STP STP ?????" ;
+                pout.println(deuxiemeContact);
+                pout.flush();
+                // récupère le code du client
+                buffin = new BufferedReader(new InputStreamReader(clientSocketOnServer.getInputStream())) ;
+                String message_distant = buffin.readLine() ;
+                System.out.println(message_distant);
+            }
+
 
             clientSocketOnServer.close();
             Thread.sleep(3000);

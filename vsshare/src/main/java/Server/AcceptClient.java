b9a587ca93d89443bd03e977562dc38eb4d76d65
Théo Choffat <theo.choffat@hes-so.ch>
2022-01-06 10:33:16 +0100

    Welcome message and login

--- vsshare/src/main/java/Server/AcceptClient.java
+++ vsshare/src/main/java/Server/AcceptClient.java
@@ -1,10 +1,7 @@
 package Server;
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class AcceptClient implements Runnable {
 
@@ -12,7 +9,12 @@ public class AcceptClient implements Runnable {
     private int clientNumber;
     private BufferedReader buffin ;
     private PrintWriter pout ;
-
+    private String delimiter = "--";
+    private int cmpt = 0;
+    private boolean codeClient = false ;
+    private String[] mdp ;
+    private int idIPAddr;
+    private int idPassword;
     //Constructor
     public AcceptClient (Socket clientSocketOnServer, int clientNo)
     {
@@ -29,40 +31,90 @@ public class AcceptClient implements Runnable {
             pout = new PrintWriter(clientSocketOnServer.getOutputStream());
 
             // va lire le fichier
-            Scanner s = new Scanner(new File("vsshare/src/main/java/RegisteredClients/TheList"));
+            Scanner s = new Scanner(new File("vsshare/src/main/java/RegisteredClients/TheList.txt"));
             ArrayList<String> list = new ArrayList<String>();
             while (s.hasNext()){
                 list.add(s.next());
+
             }
             s.close();
 
+            
+            // va stocker toutes les addresses ip en position 0 et tous les mot de passes en position 1 d'un tableau de string
+            List<String> items = null;
+            mdp = new String[list.size()*2];
+            for (String str : list){
+                items = Arrays.asList(str.split("\\s*:\\s*"));
+                System.out.println(items.toString());
+                mdp[cmpt] = items.get(0);
+                cmpt++;
+                mdp[cmpt] = items.get(1);
+                cmpt++;
+            }
+
             // va regarder si l'adresse ip existe déjà
             String ipAddr = clientSocketOnServer.getInetAddress().toString() ;
-            if(!list.contains(ipAddr)){
+            if(!items.contains(ipAddr)){
                 System.out.println("la liste n'a pas trouvé l'adresse ip");
                 // On va donc envoyer un message au client
                 String premierContact = "Oy tu as l'air nouveau par ici, je vais te créer un compte, quel mot de passe veux-tu ?" ;
                 pout.println(premierContact);
                 pout.flush();
                 // récupère le code du client
-                buffin = new BufferedReader(new InputStreamReader(clientSocketOnServer.getInputStream())) ;
-                String message_distant = buffin.readLine() ;
-                System.out.println(message_distant);
-
+                String message_distant = getInput();
+                // crée une arraylist avec le nouveau client
+                String stockage = ipAddr + ":" + message_distant;
+                // ajoute cette arraylist au fichier
+                list.add(stockage);
+                FileWriter writer = new FileWriter("vsshare/src/main/java/RegisteredClients/TheList.txt");
+                for(String str: list) {
+                    writer.write(str + System.lineSeparator());
+                }
+                writer.close();
             }
             else {
                 System.out.println("adresse ip trouvée dans le fichier");
                 // On va donc envoyer un message au client
-                String deuxiemeContact = "REBONJOUR C EST QUOI TON CODE STP STP STP ?????" ;
-                pout.println(deuxiemeContact);
-                pout.flush();
-                // récupère le code du client
-                buffin = new BufferedReader(new InputStreamReader(clientSocketOnServer.getInputStream())) ;
-                String message_distant = buffin.readLine() ;
-                System.out.println(message_distant);
+                do {
+                    String deuxiemeContact = "REBONJOUR C EST QUOI TON CODE STP STP STP ?????";
+                    pout.println(deuxiemeContact);
+                    pout.flush();
+                    // récupère le code du client
+                    String message_distant = getInput();
+                    // va chercher le code correspondant à l'adresse ip
+                    for(int i = 0 ; i < mdp.length ; i++){
+                        if(mdp[i].equals(ipAddr)){
+                            if(mdp[i+1].equals(message_distant)){
+                                // mot de passe correct
+                                idIPAddr = i ;
+                                idPassword = i+1;
+                                codeClient = true ;
+                                break;
+                            }
+                            else{
+                                // mot de passe incorrect
+                                String incorrectPassword = "TON MDP EST FAUX, T ES MAUVAIS JACK";
+                                pout.println(incorrectPassword);
+                                pout.flush();
+                            }
+                        }
+                    }
+                }while (codeClient==false);
             }
 
+            // envoie le message de bienvenue
+            pout.println(help());
+            pout.flush();
 
+            // écoute le client afin de savoir l'action à faire
+            String choice = getInput();
+
+            // 1. Send a list of all the files
+            if(choice.equals("1")){
+
+            }
+
+            // ferme le serveur
             clientSocketOnServer.close();
             Thread.sleep(3000);
             System.out.println("end of connection to the client " + clientNumber);
@@ -73,7 +125,40 @@ public class AcceptClient implements Runnable {
         }
     }
 
+    public String getInput() throws IOException {
+        pout = new PrintWriter(clientSocketOnServer.getOutputStream());
+        String initiate = "input";
+        pout.println(initiate);
+        pout.flush();
 
+        buffin = new BufferedReader(new InputStreamReader(clientSocketOnServer.getInputStream())) ;
+        String message_distant = buffin.readLine() ;
+
+        return message_distant;
+    }
+
+    public String help() throws IOException {
+        // va lire le fichier
+        Scanner s = new Scanner(new File("vsshare/src/main/java/Server/Help.txt"));
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNext()){
+            list.add(s.next());
+        }
+        s.close();
+
+        // va envoyer le message de bienvenue au client
+        String affiche="";
+        for(String str : list){
+            if(str.equals("uwu")){
+                affiche += "\n" ;
+            }
+            else {
+                affiche += str + " " ;
+            }
+        }
+
+        return affiche;
+    }
 
 
 }

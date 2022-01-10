0669e5b11a3642f227bda972f7af2ac01f976fc8
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-01-10 22:46:04 +0100

    Send Files

--- vsshare/src/main/java/Client/Client.java
+++ vsshare/src/main/java/Client/Client.java
@@ -1,76 +1,122 @@
 package Client;
 
+import javax.swing.*;
 import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Client {
 
-    private String ipAddresse;
-    private String password;
+    InetAddress serverAddress;
+    String serverName = "127.0.0.1";
+    String messageToSend = "";
+    Socket mySocket;
+    PrintWriter Pout;
 
-    public Client(String ipAddresse, String password)
-    {
-        this.ipAddresse = ipAddresse;
-        this.password = password;
+    Scanner sc;
+
+    public Client() {
+        try {
+            serverAddress = InetAddress.getByName(serverName);
+            System.out.println("Get the address of the server : " + serverAddress);
+
+            Scanner scanner = new Scanner(System.in);
+
+            mySocket = new Socket(serverAddress, 45007);
+            Gui gui = new Gui();
+            //gui.setVisible(true);
+            //gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            BufferedReader buffin = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+            Pout = new PrintWriter(mySocket.getOutputStream());
+
+            boolean stop = false;
+
+            while (!stop) {
+                //read received messgae
+
+                String message = buffin.readLine();
+
+                if (message.equals("quit")) {
+                    stop = true;
+                    break;
+                }
+
+                if (message.equals("input")) {
+                    //wait for an input from the console
+                    sc = new Scanner(System.in);
+
+                    messageToSend = sc.nextLine();
+                    Pout.println(messageToSend);
+                    Pout.flush();
+
+                }
+
+                if (messageToSend.equals("3")) {
+
+                    File file = new File("C:\\toSend\\send.txt");
+
+
+                    transferFile(file);
+                }
+
+
+                //write the message on the output stream
+                if (messageToSend.equals("quit")) {
+                    stop = true;
+                    Pout.println(messageToSend);
+                    Pout.flush();
+                    //break;
+                    System.out.println("\nTerminate client program...");
+                    mySocket.close();
+                }
+
+                System.out.println(message);
+
+
+            }
+
+            /*Client c = new Client(serverName, password);
+            Thread client = new Thread();*/
+
+           // System.out.println("We got the connexion to  " + serverAddress);
+
+            //wait a bit before exit
+            //Thread.sleep(30000);
+
+
+        } catch (
+                UnknownHostException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e);
+            System.out.println("server connection error, dying.....");
+        } catch (NullPointerException e) {
+            System.out.println("Connection interrupted with the server");
+        }
     }
 
-    public void run()
-    {
+    private void transferFile(File file) throws IOException {
 
-    }
 
-/*    public void transferFile(File source, File dest) throws IOException {
-        String filename = "file.pdf";
-        String pathname = "C://toSend//"+filename;
-        File myFile = new File(pathname);
-        long myFileSize = Files.size(Paths.get(pathname));
+        long myFileSize = Files.size(Paths.get(file.getName()));
 
-        PrintWriter Pout2 = new PrintWriter(clientSocketOnServer.getOutputStream(), true);
-        Pout2.println(myFileSize);
-        Pout2.println(filename);
+        //PrintWriter Pout = new PrintWriter(mySokcet.getOutputStream(), true);
+        Pout.println(myFileSize);
+        Pout.println(file.getName());
 
-        byte[] mybytearray = new byte[(int)myFileSize];
-        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
+        byte[] mybytearray = new byte[(int) myFileSize];
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file.getName()));
         bis.read(mybytearray, 0, mybytearray.length);
-        OutputStream os = clientSocketOnServer.getOutputStream();
+        OutputStream os = mySocket.getOutputStream();
         os.write(mybytearray, 0, mybytearray.length);
         os.flush();
-        clientSocketOnServer.close();
-        System.out.println("end of connection to the client " + clientNumber);
-    }*/
 
-
-    public void deleteFile(File file)
-    {
-        try {
-        if (file.delete())
-        {
-            System.out.println("This file was deleted");
-        }
-        }catch(Exception e)
-        {
-            System.out.println("Failed to delete the file");
-            e.printStackTrace();
-        }
-    }
-
-    public void listFiles(File f, String path)
-    {
-        String[] pathnames;
-
-        // Creates a new File instance by converting the given pathname string
-        // into an abstract pathname
-        f = new File(path);
-
-        // Populates the array with names of files and directories
-        pathnames = f.list();
-
-        // For each pathname in the pathnames array
-        for (String pathname : pathnames) {
-            // Print the names of files and directories
-            System.out.println(pathname);
-        }
     }
 }
 
